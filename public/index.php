b95ef9954b23a61cd7d6e68fb2e54e2065055bad
imed92 <92928657+imed92@users.noreply.github.com>
2025-10-30 12:31:51 +0100

    User model = vue liste users

--- public/index.php
+++ public/index.php
@@ -9,6 +9,7 @@ use Mini\Core\Router;
 // Table des routes minimaliste
 $routes = [
     ['GET', '/', [Mini\Controllers\HomeController::class, 'index']],
+    ['GET', '/users', [Mini\Controllers\HomeController::class, 'users']],
 ];
 
 // Bootstrap du router
